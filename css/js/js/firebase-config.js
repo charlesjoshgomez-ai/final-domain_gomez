f85fbcd63c0a4b644666cc8b82c641e5ecc08e2a
charlesjoshgomez-ai <charlesjosh.gomez@neu.edu.ph>
2026-03-13 22:26:54 +0800

    Update firebase-config.js

--- css/js/js/firebase-config.js
+++ css/js/js/firebase-config.js
@@ -1,11 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: 'YOUR_API_KEY_HERE',
-    authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    appId: 'YOUR_APP_ID'
+  apiKey: 'AIzaSyBM2u2Rlb5X1D658eD4-vEte54NVfLByFQ',
+  authDomain: 'neu-library-tracker.firebaseapp.com',
+  projectId: 'neu-library-tracker',
+  storageBucket: 'neu-library-tracker.firebasestorage.app',
+  messagingSenderId: '406887455810',
+  appId: '1:406887455810:web:ada680400bddd0d35ca1db',
+  measurementId: 'G-FMLE82XZ3Q'
 };
 
 // Initialize Firebase
